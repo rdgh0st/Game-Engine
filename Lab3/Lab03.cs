669e916d2fd4fd48a1b63a05e327831c4bbf8b4b
Rob Davis <83750015+rdgh0st@users.noreply.github.com>
2022-09-06 19:18:27 -0700

    Class portion of Lab3

--- Lab3/Lab03.cs
+++ Lab3/Lab03.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using CPI311.GameEngine;
 
 namespace Lab3;
 
@@ -8,6 +9,12 @@ public class Lab03 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Model model;
+    private Matrix world;
+    private Matrix view;
+    private Matrix proj;
+    private Vector3 cameraPosition = new Vector3(0, 0, 5);
+    private Vector3 modelPosition = new Vector3(0, 0, 0);
 
     public Lab03()
     {
@@ -18,7 +25,8 @@ public class Lab03 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        InputManager.Initialize();
+        Time.Initialize();
 
         base.Initialize();
     }
@@ -27,7 +35,9 @@ public class Lab03 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        model = Content.Load<Model>("Torus");
+
+
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,7 +45,49 @@ public class Lab03 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        InputManager.Update();
+        Time.Update(gameTime);
+
+        if (InputManager.IsKeyDown(Keys.W))
+        {
+            cameraPosition += Vector3.Up * Time.ElapsedGameTime * 5;
+        }
+        if (InputManager.IsKeyDown(Keys.S))
+        {
+            cameraPosition += Vector3.Down * Time.ElapsedGameTime * 5;
+        }
+        if (InputManager.IsKeyDown(Keys.A))
+        {
+            cameraPosition += Vector3.Left * Time.ElapsedGameTime * 5;
+        }
+        if (InputManager.IsKeyDown(Keys.D))
+        {
+            cameraPosition += Vector3.Right * Time.ElapsedGameTime * 5;
+        }
+        if (InputManager.IsKeyDown(Keys.Up))
+        {
+            modelPosition += Vector3.Up * Time.ElapsedGameTime * 5;
+        }
+        if (InputManager.IsKeyDown(Keys.Down))
+        {
+            modelPosition += Vector3.Down * Time.ElapsedGameTime * 5;
+        }
+        if (InputManager.IsKeyDown(Keys.Left))
+        {
+            modelPosition += Vector3.Left * Time.ElapsedGameTime * 5;
+        }
+        if (InputManager.IsKeyDown(Keys.Right))
+        {
+            modelPosition += Vector3.Right * Time.ElapsedGameTime * 5;
+        }
+
+        world = Matrix.CreateScale(1.0f) * Matrix.CreateFromYawPitchRoll(0, 0, 0) *
+                Matrix.CreateTranslation(modelPosition);
+        view = Matrix.CreateLookAt(
+            cameraPosition,
+            new Vector3(0, 0, -1),
+            new Vector3(0, 1, 0));
+        proj = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver2, 1.33f, 0.1f, 1000f);
 
         base.Update(gameTime);
     }
@@ -44,7 +96,7 @@ public class Lab03 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        model.Draw(world, view, proj);
 
         base.Draw(gameTime);
     }
