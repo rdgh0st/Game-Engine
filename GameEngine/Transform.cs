76f88cfc80a941da2db55c1a2fd2195e57543bdd
Rob Davis <83750015+rdgh0st@users.noreply.github.com>
2022-09-22 15:49:53 -0700

    Finish Lab5

--- GameEngine/Transform.cs
+++ GameEngine/Transform.cs
@@ -61,6 +61,27 @@ public class Transform
         }
     }
     
+    public Quaternion Rotation
+    {
+        get { return Quaternion.CreateFromRotationMatrix(World); }
+        set
+        {
+            if (Parent == null)LocalRotation = value;
+            else
+            {
+                Vector3 scale, pos; Quaternion rot;
+                world.Decompose(out scale, out rot, out pos);
+                Matrix total = Matrix.CreateScale(scale) *   
+                               Matrix.CreateFromQuaternion(value) * 
+                               Matrix.CreateTranslation(pos);
+                LocalRotation = Quaternion.CreateFromRotationMatrix(
+                    Matrix.Invert(Matrix.CreateScale(LocalScale))*total*
+                    Matrix.Invert(Matrix.CreateTranslation(LocalPosition) 
+                                  * Parent.world));
+            }
+        }
+    }
+    
     public Matrix World { get { return world; } }
     
     public Vector3 Forward { get { return world.Forward; } }
--- Lab5/Lab05.cs
+++ Lab5/Lab05.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using CPI311.GameEngine;
 
 namespace Lab5;
 
@@ -8,17 +9,29 @@ public class Lab05 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Effect effect;
+    private Camera camera;
+    private Model model;
+    private Model parent;
+    private Transform parentTransform;
+    private Transform modelTransform;
+    private Transform cameraTransform;
+    private int currentTechnique;
+    private SpriteFont font;
+    private string techniqueString = "Gouraud";
 
     public Lab05()
     {
         _graphics = new GraphicsDeviceManager(this);
+        _graphics.GraphicsProfile = GraphicsProfile.HiDef;
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        InputManager.Initialize();
+        Time.Initialize();
 
         base.Initialize();
     }
@@ -27,7 +40,19 @@ public class Lab05 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        effect = Content.Load<Effect>("Shader");
+        model = Content.Load<Model>("Torus");
+        parent = Content.Load<Model>("Torus");
+        font = Content.Load<SpriteFont>("font");
+        modelTransform = new Transform();
+        parentTransform = new Transform();
+        cameraTransform = new Transform();
+        cameraTransform.LocalPosition = Vector3.Backward * 5;
+        camera = new Camera();
+        camera.Transform = cameraTransform;
+
+        modelTransform.Parent = parentTransform;
+        modelTransform.LocalPosition += new Vector3(3, 0, 0);
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,7 +60,35 @@ public class Lab05 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        InputManager.Update();
+        Time.Update(gameTime);
+
+        if (InputManager.IsKeyPressed(Keys.D1))
+        {
+            currentTechnique = 0;
+            techniqueString = "Gouraud";
+        } else if (InputManager.IsKeyPressed(Keys.D2))
+        {
+            currentTechnique = 1;
+            techniqueString = "Phong";
+        } else if (InputManager.IsKeyPressed(Keys.D3))
+        {
+            currentTechnique = 2;
+            techniqueString = "Phong-Blinn";
+        } else if (InputManager.IsKeyPressed(Keys.D4))
+        {
+            currentTechnique = 3;
+            techniqueString = "Schlick";
+        }
+        
+        if(InputManager.IsKeyDown(Keys.W))
+            cameraTransform.LocalPosition += cameraTransform.Forward * Time.ElapsedGameTime;
+        if(InputManager.IsKeyDown(Keys.S))
+            cameraTransform.LocalPosition += cameraTransform.Backward * Time.ElapsedGameTime;
+        if(InputManager.IsKeyDown(Keys.A))
+            cameraTransform.Rotate(Vector3.Up, Time.ElapsedGameTime);
+        if(InputManager.IsKeyDown(Keys.D))
+            cameraTransform.Rotate(Vector3.Down, Time.ElapsedGameTime);
 
         base.Update(gameTime);
     }
@@ -44,7 +97,38 @@ public class Lab05 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        Matrix view = camera.View;
+        Matrix projection = camera.Projection;
+ 
+        effect.CurrentTechnique = effect.Techniques[currentTechnique]; //"0" is the first technique
+        effect.Parameters["World"].SetValue(parentTransform.World);
+        effect.Parameters["View"].SetValue(view);
+        effect.Parameters["Projection"].SetValue(projection);
+        effect.Parameters["LightPosition"].SetValue( Vector3.Backward * 10 + 
+                                                     Vector3.Right * 5);
+        effect.Parameters["CameraPosition"].SetValue(cameraTransform.Position);
+        effect.Parameters["Shininess"].SetValue(20f);
+        effect.Parameters["AmbientColor"].SetValue(new Vector3(0.2f, 0.2f, 0.2f));
+        effect.Parameters["DiffuseColor"].SetValue(new Vector3(0.5f, 0, 0));
+        effect.Parameters["SpecularColor"].SetValue(new Vector3(0, 0, 0.5f));
+        //effect.Parameters["DiffuseTexture"].SetValue(texture);
+        foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+        {
+            pass.Apply();
+            foreach (ModelMesh mesh in model.Meshes)
+            foreach (ModelMeshPart part in mesh.MeshParts)
+            {
+                GraphicsDevice.SetVertexBuffer(part.VertexBuffer);
+                GraphicsDevice.Indices = part.IndexBuffer;
+                GraphicsDevice.DrawIndexedPrimitives(
+                    PrimitiveType.TriangleList, part.VertexOffset, 0,
+                    part.NumVertices, part.StartIndex, part.PrimitiveCount);
+            }
+        }
+        
+        _spriteBatch.Begin();
+        _spriteBatch.DrawString(font, techniqueString, new Vector2(50, 50), Color.White);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
